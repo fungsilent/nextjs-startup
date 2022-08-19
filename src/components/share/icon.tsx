2076b3bd29508a5f8f72ded042be1463082a6d66
fungsilent <funglee162@gmail.com>
2022-08-19 16:22:42 +0800

    update share component: Icon

--- src/components/share/icon.tsx
+++ src/components/share/icon.tsx
@@ -1,15 +1,20 @@
-import Image from '@/components/share/image'
+import Image from 'next/image'
 import { setClassName } from '@/utils'
 import styles from '@/styles/share/icon.module.scss'
 
-interface Props {
+type Props = {
     src: string
     alt?: string
     className?: string
     [restProps: string]: any
 }
 
-const Icon = ({ src, alt = '', className = '', ...rest }: Props) => {
+const Icon = ({
+    src,
+    alt = '',
+    className = '',
+    ...rest
+}: Props) => {
     return (
         <div className={setClassName([styles.icon, className])} {...rest}>
             <i>
@@ -18,6 +23,7 @@ const Icon = ({ src, alt = '', className = '', ...rest }: Props) => {
                     alt={alt}
                     layout='fill'
                     priority={true}
+                    objectFit='contain'
                 />
             </i>
         </div>
