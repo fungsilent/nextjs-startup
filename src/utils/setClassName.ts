f5c3e7f6ba7972906733bc17424c639b743ceb43
fungsilent <funglee162@gmail.com>
2022-08-24 13:38:37 +0800

    update util function: setClassName

--- src/utils/setClassName.ts
+++ src/utils/setClassName.ts
@@ -3,9 +3,9 @@ import { map, isArray } from 'lodash'
 const setClassName = (classNames: (string | [boolean, string])[]): string => {
     return map(classNames, name => {
         if (isArray(name)) {
-            return name[0] ? name[1] : ''
+            return !!name[0] ? name[1] : ''
         } else {
-            return name
+            return name ?? ''
         }
     }).join(' ')
 }
