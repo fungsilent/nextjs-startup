5fb591c7bf1e3c7af608c743228c2458488376b0
fungsilent <funglee162@gmail.com>
2022-08-26 16:13:02 +0800

    update example: SEO

--- src/pages/example/seo.tsx
+++ src/pages/example/seo.tsx
@@ -1,33 +1,36 @@
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import _ from 'lodash'
+import axios from 'axios'
 import Display from '@/components/page/example/display'
 import styles from '@/styles/page/example.module.scss'
 
-export default function SEO({ response, title, description }) {
+const SEO = ({ response, title, description }) => {
     console.log(response)
     return (
         <div className={styles.layout}>
-            <Head>
-                <title>{title}</title>
-                <meta name='description' content={description} />
-            </Head>
+            <NextSeo
+                title={title}
+                description={description}
+            />
             <h2>SEO</h2>
             <Display v={{ title }}/>
             <Display v={{ description }}/>
         </div>
     )
-  }
+}
   
-export async function getServerSideProps() {
-    const response = await fetch('https://restcountries.com/v3.1/all');
-    const demoData = await response.json();
-    const c = _.sample(demoData)
-  
+export const getServerSideProps = async () => {
+    const response = await axios.get('https://restcountries.com/v3.1/all')
+    const c = _.sample(response.data)
     return {
         props: {
-            response: c,
-            title: c.altSpellings[0],
-            description: _.last(c.altSpellings),
+            page: {
+                response: c,
+                title: c.altSpellings[0],
+                description: _.last(c.altSpellings),
+            }
         },
     }
 }
+
+export default SEO
