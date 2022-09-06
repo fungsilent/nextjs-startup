0693802cb59beba31ce44f3e798b7751b627b6d9
fungsilent <funglee162@gmail.com>
2022-09-06 15:58:04 +0800

    update hook: useStep

--- src/hooks/useStep.ts
+++ src/hooks/useStep.ts
@@ -1,22 +1,19 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
-const useStep = (
-    size: number,
-    start?: number
-) => {
+const useStep = (size: number, start?: number) => {
     const [step, setStep] = useState(start || 0)
 
-    const nextStep = () => {
+    const nextStep = useCallback(() => {
         if (step < size - 1) {
             setStep(prevStep => prevStep + 1)
         }
-    }
+    }, [setStep])
 
-    const prevStep = () => {
+    const prevStep = useCallback(() => {
         if (step > 0) {
             setStep(prevStep => prevStep - 1)
         }
-    }
+    }, [setStep])
 
     return {
         step,
